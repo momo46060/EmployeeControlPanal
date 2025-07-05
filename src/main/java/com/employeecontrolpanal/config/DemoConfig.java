a50ba22c41d0a3fde6c90c8c7b1dbcc99bb3708e
Mohamed Ahmed <77528820+momo46060@users.noreply.github.com>
2025-07-05 13:04:46 +0300

    Add Users from Custom tables

--- src/main/java/com/employeecontrolpanal/config/DemoConfig.java
+++ src/main/java/com/employeecontrolpanal/config/DemoConfig.java
@@ -6,29 +6,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoConfig {
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123") // {noop} indicates no password encoding
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123") // {noop} indicates no password encoding
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123") // {noop} indicates no password encoding
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager inMemoryUserDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager
+                .setUsersByUsernameQuery("select username,password,enabled from members where username=?");
+        jdbcUserDetailsManager
+                .setAuthoritiesByUsernameQuery("select username ,role from roles where username=?");
+        return  jdbcUserDetailsManager;
     }
 
     @Bean
